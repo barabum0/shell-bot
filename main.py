c7baa66953ed9b4a69043ec917d0e2b5bd4c1516
roman mac <barabum@duck.com>
2023-11-15 13:43:52 +0300

    :sparkles: run

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import asyncio
 
-from aiogram import Bot
+from aiogram import Bot, Dispatcher
 
 from services.config import load_config
+from services.routers import custom
 
 config = load_config()
 bot = Bot(token=config.bot_token, parse_mode="MarkdownV2")
+dispatcher = Dispatcher()
 
 
-async def main() -> None:
-    pass
+async def setup() -> None:
+    dispatcher.include_router(custom.router)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(setup())
+    dispatcher.run_polling(bot)
