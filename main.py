6d21daf9e1c21001eb4f270f35387571237b101a
roman mac <barabum@duck.com>
2023-11-20 14:25:28 +0300

    :recycle: refactor(main): Update setup function to return Bot and Dispatcher objects

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ from loguru import logger
 from services.config import load_config
 from services.routers import custom, default
 
-config = load_config()
-bot = Bot(token=config.bot_token, parse_mode="MarkdownV2")
-dispatcher = Dispatcher(config=config)
 
+async def setup() -> tuple[Bot, Dispatcher]:
+    config = load_config()
+    bot = Bot(token=config.bot_token, parse_mode="MarkdownV2")
+    dispatcher = Dispatcher(config=config)
 
-async def setup() -> None:
     routers = [
         custom.router,
         default.router
     ]
     dispatcher.include_routers(*routers)
+    return bot, dispatcher
 
 
 if __name__ == '__main__':
     logger.info("Starting bot...")
-    asyncio.run(setup())
+    bot, dispatcher = asyncio.run(setup())
     dispatcher.run_polling(bot)
     logger.info("Stopped!")
