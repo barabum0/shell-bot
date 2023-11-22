8264a0e3cc61cfb14e1e27cd4bb07762c769e954
roman mac <barabum@duck.com>
2023-11-22 15:11:28 +0300

    :recycle: refactor: simplify custom command handling and regex usage
    - Replace `regex` with `re` for command text extraction
    - Streamline custom command handling logic
    - Remove unnecessary message mention check and confirmation logic
    - Standardize log and error message formatting
    - Ensure output is sent correctly and handle large outputs with document sendMessage
    - Remove unused imports and code paths

--- services/routers/custom.py
+++ services/routers/custom.py
@@ -1,87 +1,58 @@
 import os
-
-import regex
+import re
 from aiogram import Router, F, Bot
 from aiogram.exceptions import TelegramBadRequest
-from aiogram.types import Message, BufferedInputFile, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from aiogram.types import Message, BufferedInputFile, InlineKeyboardMarkup, InlineKeyboardButton
 from loguru import logger
 
-from services.config import Config, defaults
-from services.routers import escape_markdown
+from services.config import Config
 
 router = Router()
 
 
-@router.message(
-    F.func(
-        lambda message: message.text.startswith("/") and not any(message.text.startswith(d) for d in defaults)
-    )
-)
-async def custom_command(
-        message: Message = None,
-        config: Config = None,
-        confirmed: bool = False,
-        confirmation_message: Message = None,
-        bot: Bot = None,
-        confirmation_command: str = None) -> None:
-    if message and config.whitelisted_chat_ids and message.chat.id not in config.whitelisted_chat_ids:
-        logger.error("{chat_id} not in whitelisted chats", chat_id=message.chat.id)
+@router.message(F.text.startswith("/"))
+async def custom_command(message: Message, config: Config, bot: Bot) -> None:
+    # Check if chat is whitelisted and mention check for non-private chats is skipped for simplicity
+    if config.whitelisted_chat_ids and message.chat.id not in config.whitelisted_chat_ids:
+        logger.error("Chat {chat_id} not in whitelisted chats", chat_id=message.chat.id)
         return
-    if message and message.chat.type != "private" and not confirmation_command:
-        me = await bot.get_me()
-        if not message.text.endswith(f"@{me.username}"):
-            logger.error("Not mentioned the bot")
-            return
 
-    if not confirmation_command:
-        command_text = regex.match("(?P<command>/[^@ ]*)", message.text).group("command")
-    else:
-        command_text = confirmation_command
+    # Extract command text
+    command_text = re.match(r"/[^@\s]+", message.text).group()
     command = config.shells.get(command_text)
 
-    if command.need_confirmation and not confirmed:
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[[
-            InlineKeyboardButton(text="✅ Yes", callback_data=f"confirm_yes_{command_text}"),
-            InlineKeyboardButton(text="⛔️ No", callback_data=f"confirm_no_{command_text}")
-        ]])
-
-        await message.reply(f"*Are you sure you want to run *`{message.text}`*?*", reply_markup=keyboard)
+    # Handle command confirmation if needed
+    if command.need_confirmation and not hasattr(message, 'is_confirmed'):
+        keyboard = InlineKeyboardMarkup(inline_keyboard=[
+            [InlineKeyboardButton(text="✅ Yes", callback_data=f"confirm_yes_{command_text}"),
+             InlineKeyboardButton(text="⛔️ No", callback_data=f"confirm_no_{command_text}")]
+        ])
+        await message.reply(f"Are you sure you want to run `{message.text}`?", reply_markup=keyboard)
         return
-    elif command.need_confirmation and confirmed:
-        m = confirmation_message
-        await m.edit_text(command.loading_message)
-    else:
-        m = await message.reply(command.loading_message)
 
-    result = os.popen(command.shell).read()
-    output = f"{command.output_message}"
-
-    result = escape_markdown(result)
+    # Execute the command
+    result = os.popen(command.shell).read().strip()
     if command.send_output:
-        output_with_result = f"{output}\n\n```shell\n{result}\n```"
+        output_with_result = f"{command.output_message}\n\n```\n{result}\n```"
     else:
-        output_with_result = output
+        output_with_result = command.output_message
 
     try:
-        await m.edit_text(output_with_result)
+        await bot.send_message(message.chat.id, output_with_result, parse_mode='Markdown')
     except TelegramBadRequest:
-        await m.delete()
-        await m.reply_document(document=BufferedInputFile(result.encode("utf-8"), filename="output.txt"), caption=output)
+        await bot.send_document(message.chat.id, BufferedInputFile(result.encode(), "output.txt"), caption=output_with_result)
 
 
-@router.callback_query(F.data.startswith(f"confirm_"))
-async def confirm_command(callback_query: CallbackQuery, config: Config):
+@router.callback_query(F.data.startswith("confirm_"))
+async def confirm_command(callback_query, config: Config, bot: Bot) -> None:
     if config.whitelisted_chat_ids and callback_query.message.chat.id not in config.whitelisted_chat_ids:
         return
 
-    _, choice, *command = callback_query.data.split("_")
-
-    command = "_".join(command)
-    if choice == "no":
-        await callback_query.message.delete()
-        return
+    choice, command_text = callback_query.data.split("_")[1:3]
+    await callback_query.message.delete()
 
     if choice == "yes":
-        return await custom_command(confirmed=True,
-                                    confirmation_message=callback_query.message,
-                                    confirmation_command=command, config=config)
+        message = callback_query.message
+        setattr(message, 'is_confirmed', True)
+        await custom_command(message, config, bot)
+        
