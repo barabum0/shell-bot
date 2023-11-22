5a6f7d6289f3d5ea035369de6786cc577fbb49f6
roman mac <barabum@duck.com>
2023-11-22 14:52:14 +0300

    :bug: fix: ensure /help command respects config toggle and logs errors

--- services/routers/default.py
+++ services/routers/default.py
@@ -1,13 +1,17 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from loguru import logger
 
 from services.config import Config
 
 router = Router()
 
 
-@router.message(F.text.startswith("/help") & F.config.default_commands.help)
+@router.message(F.text.startswith("/help"))
 async def help_command(message: Message, config: Config):
+    if not config.default_commands.help:
+        logger.error("/help was called but is not enabled")
+        return
     text = "*Your shells:*\n\n"
     text += "\n".join(
         f"\- `{k}`\n{v.description}" for k, v in config.shells.items()
