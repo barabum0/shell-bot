a1a8f3c47a1e269ef158c66661af9e387c2d8d9f
sushka <r.m.poltorabatko@gmail.com>
2024-05-31 13:08:44 +0300

    ✨ feat: добавил получение информации о боте в /help команде

--- shell_bot/routers/default.py
+++ shell_bot/routers/default.py
@@ -1,4 +1,4 @@
-from aiogram import F, Router
+from aiogram import Bot, F, Router
 from aiogram.types import Message
 from loguru import logger
 
@@ -8,10 +8,16 @@ router = Router()
 
 
 @router.message(F.text.startswith("/help"))
-async def help_command(message: Message, config: Config) -> None:
+async def help_command(message: Message, config: Config, bot: Bot) -> None:
     if not config.default_commands.help:
         logger.error("/help was called but is not enabled")
         return
+    me = await bot.get_me()
+    assert me.username is not None
+
     text = "*Your shells:*\n\n"
-    text += "\n".join(f"\\- `{k}`\n{v.description}" for k, v in config.shells.items())
+    text += "\n".join(
+        f"\\- `{k}{f"@{me.username}" if config.prevent_unmentioned_commands_in_groups else ""}`\n{v.description}"
+        for k, v in config.shells.items()
+    )
     await message.reply(text)
